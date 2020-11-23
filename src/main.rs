549ab5643611c832e37f2b03751d36c34b522873
Michal Honc <michal.honc@usetreno.cz>
2020-11-23 08:47:53 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    pattern: String,
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let result = std::fs::read_to_string("test.txt");
+    let content = match result {
+        Ok(content) => { content },
+        Err(error) => { return Err(error.into()); }
+    };
+    println!("file content: {}", content);
+    Ok(())
+}
